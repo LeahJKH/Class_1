247ffa2962d8a5e8f33cf8d83bfef733a13f39bc
LeahJKH <Leahjb06.kodehode@gmail.com>
2026-02-17 13:40:27 +0100

    time 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const variabel2 = "hade"
 variabel = "heisann"
 
     // ariabel2 = "hade"
-//string, tall, array, boolen, object, litteral string
+//string, tall, array, boolen, object, litteral string, Null, Undefined.
 let string = "dette er en string"
 let tall = 2347
 let array = ["denne", "har", "flere", "strings"]
@@ -26,10 +26,21 @@ let litteralString = `denne kan ha i ${object.key3}`
 // sammenkoble
 
 //operatorer
-// +, -, *, /, %
+// +, -, *, /, %, ==, ===, !=, =
 // console.log(101 % 10)
 // console.log("hei" + " " + variabel)
 
+// = definere
+// == betyr at verdien er det samme
+// === verdien er det samme men type er det samme
+// != not equals too
+
+// +=, -=, /=, %=
+
+let sum = 5
+
+sum += 5
+// console.log(sum)
 let favorittBand = "gorillaz"
 // console.log(favorittBand)
 favorittBand = "Nirvana"
@@ -73,3 +84,96 @@ function start() {
 // }
 
 start()
+
+// putt antall på ingridienser og legg di inn i brownies
+let ingridents = ["egg", "hvetemel", "melk", "sjokolade pulver", "vanilje sukker", "sukker"]
+let brownies = []
+
+
+if (ingridents[0] == "egg") {
+    let antallEgg = "4 "
+    antallEgg += ingridents[0]
+    // console.log(antallEgg)  
+    brownies.push(antallEgg)
+}
+if (ingridents[1] == "hvetemel") {
+    let antallMel = "3.5dl "
+    antallMel += ingridents[1]
+    brownies.push(antallMel)
+
+}
+if (ingridents[2] == "melk") {
+    let antallMelk = "1dl "
+    antallMelk += ingridents[2]
+    brownies.push(antallMelk)
+
+}
+if (ingridents[3] == "sjokolade pulver") {
+    let antallSP = "5s "
+    antallSP += ingridents[3]
+    brownies.push(antallSP)
+
+}
+if (ingridents[4] == "vanilje sukker") {
+    let antallVS = "4ts"
+    antallVS += ingridents[4]
+    brownies.push(antallVS)
+    
+}
+if (ingridents[5] == "sukker") {
+    let antallS = "4.5dl "
+    antallS += ingridents[5]
+    brownies.push(antallS)
+    
+}
+// console.log(typeof brownies)
+
+// console.log(ingridents)
+// console.log(brownies)
+if (brownies.length <= 5) {
+    // console.log("cake is not done")
+} else {
+    // console.log("you have everything")
+    bake()
+}
+
+
+function bake() {
+    // console.log("baking....")
+    brownies = "delicious brownies"
+    // console.log(brownies)
+}
+// let mystring = 1 
+// // 1
+// mystring++
+// // 2
+
+// // 2
+// ++mystring
+// // 2
+
+// ++ = + 1
+
+// += = variable + noe annet
+
+// console.log(mystring)
+// {
+//     name: "Meta Quest 3S 128GB",
+//     ratings: 4,
+//     pictures: 
+//         ["url", "url", "url"],
+//     price: 3390,
+//     color: undefined,
+//     size: ["128 gb", "256 gb"]
+// }
+
+
+let stringB = ""
+
+++stringB
+// console.log(stringB, typeof stringB)
+
+for (let i = "0"; i != "3"; i++) {
+    console.log(i)
+    
+}
